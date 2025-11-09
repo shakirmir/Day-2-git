1bb772107c3948f3aa1961d66aa837447fcf87b6
shakirmir <39324287+shakirmir@users.noreply.github.com>
2025-11-09 13:12:20 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,6 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello batch40 team with Isar , Noman Amitabh");
+      System.out.println("Hello Batch batch40 WITH FOR STUDENTS RUNNING in front of me and Myusr, Rohit, 3rd Commit Aditya in making CI automated vikas teach devops CI proecss");
+    }  
+} 
